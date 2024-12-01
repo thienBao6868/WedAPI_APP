873a7fb19738078923f2457972a0aded0546a82d
ThienBao <thienbao3018@gmail.com>
2024-12-01 10:55:41 +0700

    create first Controller

---
+++ WedAPI_APP/WedAPI_APP/Controllers/MerchandiseController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WedAPI_APP.Models;
+
+namespace WedAPI_APP.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MerchandiseController : ControllerBase
+    {
+        public static List<Merchandise> merchandises = new List<Merchandise>();
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(merchandises);
+        }
+    }
+}
---
+++ WedAPI_APP/WedAPI_APP/Models/Merchandise.cs
@@ -0,0 +1,12 @@
+﻿namespace WedAPI_APP.Models
+{
+    public class MerchandiseVM
+    {
+        public string? MerchandiseName { get; set; }
+        public double Prices { get; set; }
+    }
+    public class Merchandise : MerchandiseVM
+    { 
+        public int MerchandiseCode { get; set; }
+    }
+}
